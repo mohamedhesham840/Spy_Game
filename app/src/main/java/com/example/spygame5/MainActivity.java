1992fc7a13cec6117bd993ca425f1be0382f93e5
Mohamed Hisham <mohamedhesham840@gmail.com>
2022-08-21 21:41:16 +0200

    ana bt8yr

--- app/src/main/java/com/example/spygame5/MainActivity.java
+++ app/src/main/java/com/example/spygame5/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i=new Intent(getBaseContext(),HomeActivity.class);
                 startActivity(i);
+                //hi ana lsa mt8yer
             }
         });
     }
