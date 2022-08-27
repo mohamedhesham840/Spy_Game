7a6364e3bf678563dc1017c6b189adc1110b68cb
moo <mohamedparker444@gmail.com>
2022-08-27 22:07:37 +0200

    backEnd 8(with sound)

--- app/src/main/java/com/example/spygame5/MainActivity.java
+++ app/src/main/java/com/example/spygame5/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.spygame5;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -15,7 +17,14 @@ public class MainActivity extends AppCompatActivity {
     private ImageView info_imv;
     private  ImageView log_imv;
     private TextView  rules_tv;
+
     private boolean   visible = false; // for appear/disappear rules text view
+
+    // audio
+    private  static MediaPlayer mediaPlayer;
+    private static ImageView audioPlay_tv;
+    private static ImageView audioPause_tv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +36,8 @@ public class MainActivity extends AppCompatActivity {
         info_imv= findViewById(R.id.info_id);
         log_imv = findViewById(R.id.log_id);
         rules_tv= findViewById(R.id.rules_id) ;
-
+        audioPause_tv= findViewById(R.id.audioPause_id);
+        audioPlay_tv =findViewById(R.id.audioPlay_id);
 
         // events
         // navigate to home activity
@@ -58,5 +68,37 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
     }
+
+    public static void audioPlay(View v){
+        if(mediaPlayer == null){
+            mediaPlayer = MediaPlayer.create(v.getContext(), R.raw.audio2);
+        }
+        mediaPlayer.start();
+        mediaPlayer.setLooping(true);
+        audioPause_tv.setVisibility(View.INVISIBLE);
+        audioPlay_tv.setVisibility(View.VISIBLE);
+
+
+
+    }
+
+    public static void audioPause(View v){
+    if(mediaPlayer != null){
+        mediaPlayer.pause();
+        audioPause_tv.setVisibility(View.VISIBLE);
+        audioPlay_tv.setVisibility(View.INVISIBLE);
+    }
+    }
+    public static void audioStop(){
+        if(mediaPlayer != null){
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+        audioPause_tv.setVisibility(View.VISIBLE);
+        audioPlay_tv.setVisibility(View.INVISIBLE);
+    }
+
 }
